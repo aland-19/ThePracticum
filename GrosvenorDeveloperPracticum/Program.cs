2671b0fde514c3a318aeb51cdf719df39f123942
Boris Barvish <bbarvish@gmai.com>
2023-09-30 15:39:05 -0700

    Initial commit with skeleton project to move forward with

---
+++ GrosvenorDeveloperPracticum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Application;
+
+namespace GrosvenorInHousePracticum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var server = new Server(new DishManager());
+            while (true)
+            {
+                Console.WriteLine("Please provide your order:");
+                var unparsedOrder = Console.ReadLine();
+                var output = server.TakeOrder(unparsedOrder);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
