d91be6b20c4f95038f6324f81c89c6096bebecc8
Boris Barvish <bbarvish@gmail.com>
2024-02-08 17:58:06 -0700

    deleted a line and changed one

--- Application/Dish.cs
+++ Application/Dish.cs
@@ -2,8 +2,7 @@
 {
     /// <summary>
     /// Contains a dish by name and number of times the dish has been ordered
-
-    /// This line has no value at all because no one cares about it
+    /// This line has no value at all because no one cares about it!
     ///
     /// Making changes
     /// This line has no value at all
