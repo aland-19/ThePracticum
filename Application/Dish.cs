912b36a2bc81cbfc087f2334e4ee8401bd12e9de
Boris Barvish <bbarvish@gmail.com>
2024-02-08 17:50:29 -0700

    more meaningless changes for the fun of git!

--- Application/Dish.cs
+++ Application/Dish.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Contains a dish by name and number of times the dish has been ordered
-    /// This line has no value at all
+    /// This line has no value at all because no one cares about it
     /// </summary>
     public class Dish
     {
